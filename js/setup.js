6a076fa6291edf33cf572858c6606f0987e028b2
Kenan Agaev <kenkosh@yandex.ru>
2020-02-01 18:25:15 +0300

    Выполняет задание 1

--- js/setup.js
+++ js/setup.js
@@ -2,6 +2,8 @@
 'use strict';
 
 var SIMILAR_WIZARDS_COUNT = 4;
+var ESC_KEY = 'Escape';
+var ENTER_KEY = 'Enter';
 
 var firstNames = [
   'Иван',
@@ -41,6 +43,32 @@ var eyesColors = [
   'yellow',
   'green'
 ];
+
+var fireballColors = [
+  '#ee4830',
+  '#30a8ee',
+  '#5ce6c0',
+  '#e848d5',
+  '#e6e848'
+];
+
+// находит окно настроек персонажа
+var setup = document.querySelector('.setup');
+var setupOpen = document.querySelector('.setup-open');
+var setupClose = setup.querySelector('.setup-close');
+var titleInput = setup.querySelector('.setup-user-name');
+
+// находит модель персонажа мага
+var wizardModel = document.querySelector('.setup-wizard');
+var wizardCoat = wizardModel.querySelector('.wizard-coat');
+var wizardEyes = wizardModel.querySelector('.wizard-eyes');
+var wizardFireball = document.querySelector('.setup-fireball-wrap');
+
+// находит скрытые инпуты
+var playerSetup = document.querySelector('.setup-player');
+var coatInput = playerSetup.querySelector('input[name="coat-color"]');
+var eyesInput = playerSetup.querySelector('input[name="eyes-color"]');
+var fireballInput = playerSetup.querySelector('input[name="fireball-color"]');
 
 // находит контейнер для похожих магов
 var similarListContainer = document.querySelector('.setup-similar-list');
@@ -101,7 +129,7 @@ var setWizard = function (elem, array) {
   elem.querySelector('.wizard-eyes').style.fill = array.eyeColor;
 };
 
-// отрисовывает магов
+// получает фрагмент магов
 var getFragment = function (count) {
   var fragment = document.createDocumentFragment();
 
@@ -114,9 +142,86 @@ var getFragment = function (count) {
   return fragment;
 };
 
-// показывает необходимые окна
-document.querySelector('.setup').classList.remove('hidden');
-document.querySelector('.setup-similar').classList.remove('hidden');
+// изменяет цвет и знчение атрибута скрытого инпута
+var changeColor = function (array, input) {
+  var color = array[getRandomInt(0, getMaxIndex(array))];
+  input.value = color;
+
+  return color;
+};
+
+// обработчики событий
+var onPopupEscPress = function (evt) {
+  if (evt.key === ESC_KEY) {
+    closePopup();
+  }
+};
+
+var onInputEscStopProp = function (evt) {
+  if (evt.key === ESC_KEY) {
+    evt.stopPropagation();
+  }
+};
+
+var onSetupOpenEnterPress = function (evt) {
+  if (evt.key === ENTER_KEY) {
+    openPopup();
+  }
+};
+
+var onSetupCloseEnterPress = function (evt) {
+  if (evt.key === ENTER_KEY) {
+    closePopup();
+  }
+};
+
+// открывает окно настроек персонажа
+var openPopup = function () {
+  setup.classList.remove('hidden');
+  document.addEventListener('keydown', onPopupEscPress);
+};
+
+// закрывает окно настроек персонажа
+var closePopup = function () {
+  setup.classList.add('hidden');
+  document.removeEventListener('keydown', onPopupEscPress);
+};
+
+setupOpen.addEventListener('click', function () {
+  openPopup();
+});
+
+setupOpen.addEventListener('keydown', function (evt) {
+  onSetupOpenEnterPress(evt);
+});
+
+setupClose.addEventListener('click', function () {
+  closePopup();
+});
+
+setupClose.addEventListener('keydown', function (evt) {
+  onSetupCloseEnterPress(evt);
+});
+
+titleInput.addEventListener('keydown', function (evt) {
+  onInputEscStopProp(evt);
+});
+
+// обработчики событий на модели мага
+wizardCoat.addEventListener('click', function () {
+  wizardCoat.style.fill = changeColor(coatsColors, coatInput);
+});
+
+wizardEyes.addEventListener('click', function () {
+  wizardEyes.style.fill = changeColor(eyesColors, eyesInput);
+});
+
+wizardFireball.addEventListener('click', function () {
+  wizardFireball.style.backgroundColor = changeColor(fireballColors, fireballInput);
+});
+
+// действия
+setup.querySelector('.setup-similar').classList.remove('hidden');
 
 var wizards = createWizardsList(SIMILAR_WIZARDS_COUNT);
 var wizardsFragment = getFragment(SIMILAR_WIZARDS_COUNT);
