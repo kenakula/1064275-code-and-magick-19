89077cd6d42b2d815d6b8aeb3c55f9868f70eb8c
Kenan Agaev <kenkosh@yandex.ru>
2020-02-16 14:54:26 +0300

    Настраивает получение и отправку данных на сервер

---
+++ js/backend.js
@@ -0,0 +1,64 @@
+/* eslint-disable no-console */
+'use strict';
+
+(function () {
+  var TIMEOUT_IN_MS = 10000; // 10 s
+  var RESPONSE_TYPE = 'json';
+  var DOWNLOAD_LINK = 'https://js.dump.academy/code-and-magick/data';
+  var UPLOAD_LINK = 'https://js.dump.academy/code-and-magick';
+  var responseCode = {
+    OK: 200,
+    notFound: 404,
+    Unavailable: 503,
+    GatewayTimeout: 504,
+  };
+  window.backend = {};
+
+  var responseHandler = function (xhr, onLoad, onError) {
+    xhr.addEventListener('load', function () {
+      switch (xhr.status) {
+        case responseCode.OK:
+          onLoad(xhr.response);
+          console.log(xhr.status);
+          break;
+        case responseCode.notFound:
+          onError('Запрашиваемыe данные не существуют!');
+          break;
+        case responseCode.Unavailable:
+          onError('Ошибка сервера, побробуйте снова позже');
+          break;
+        case responseCode.GatewayTimeout:
+          onError('Слишком долгое ожидание ответа сервера, возможно медленное интернет-соединение');
+          break;
+        default:
+          onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
+      }
+    });
+
+    xhr.addEventListener('timeout', function () {
+      onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
+    });
+  };
+
+  window.backend.load = function (onLoad, onError) {
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = RESPONSE_TYPE;
+    xhr.timeout = TIMEOUT_IN_MS;
+
+    responseHandler(xhr, onLoad, onError);
+
+    xhr.open('GET', DOWNLOAD_LINK);
+    xhr.send();
+  };
+
+  window.backend.save = function (data, onLoad, onError) {
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = RESPONSE_TYPE;
+
+    responseHandler(xhr, onLoad, onError);
+
+    xhr.open('POST', UPLOAD_LINK);
+    xhr.send(data);
+  };
+
+})();
--- js/dialog.js
+++ js/dialog.js
@@ -8,6 +8,8 @@
   var setupOpen = document.querySelector('.setup-open');
   var setupClose = setup.querySelector('.setup-close');
   var titleInput = setup.querySelector('.setup-user-name');
+  var form = setup.querySelector('.setup-wizard-form');
+  var submitButton = form.querySelector('.setup-submit');
 
   var resetPopupPosition = function () {
     setup.style.top = '';
@@ -44,6 +46,25 @@
     document.removeEventListener('keydown', onEscPress);
   };
 
+  var successHandler = function () {
+    submitButton.textContent = 'Сохранить';
+    submitButton.disabled = false;
+    setup.classList.add('hidden');
+  };
+
+  var errorHandler = function (errorMessage) {
+    var node = document.createElement('div');
+
+    node.style = 'z-index: 100; margin: 0 auto; text-align: center; background-color: red;';
+    node.style.position = 'absolute';
+    node.style.left = 0;
+    node.style.right = 0;
+    node.style.fontSize = '30px';
+
+    node.textContent = errorMessage;
+    document.body.insertAdjacentElement('afterbegin', node);
+  };
+
   setupOpen.addEventListener('click', function () {
     openPopup();
   });
@@ -59,6 +80,13 @@
 
   setupClose.addEventListener('keydown', function (evt) {
     onSetupCloseEnterPress(evt);
+  });
+
+  form.addEventListener('submit', function (evt) {
+    submitButton.textContent = 'Данные отправляются';
+    submitButton.disabled = true;
+    window.backend.save(new FormData(form), successHandler, errorHandler);
+    evt.preventDefault();
   });
 
 })();
